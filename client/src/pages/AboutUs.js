643ba56d6f4be62ef871d0f0e427b05184d8b84c
sweave19 <sweaver1997@yahoo.com>
2019-07-22 20:45:28 -0500

    fixed error

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -40,8 +40,15 @@ const Background = Styled.main`
 const AboutUs = (props) => {
   console.log(props);
   const { classes } = props;
-
-
+  return (
+    <Background>
+      <NavWrapperDiv>
+        <SideBar />
+      </NavWrapperDiv>
+      <Wrapper>
+        <Card className={classes.card}>
+          <CardContent>
+            <h3>Our Inspiration</h3>
           <p>
           Everybody is born different, because of those differences, unfortunately, some children
           are isolated, shunned, bullied and made fun of through no fault of their own — it's not ok.
